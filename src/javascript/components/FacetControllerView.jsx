3830ba2c30483c85562c0ff1998a564bd63cdaa0
Francisco Martinez <FrankyMartz@gmail.com>
2014-12-02 08:31:35 -0600

    [UPDATE](FacetControllerView.jsx)State, Listeners
    Include Helper functions to manage, disperse data store amongst its
    children. Setup Listeners.

--- src/javascript/components/FacetControllerView.jsx
+++ src/javascript/components/FacetControllerView.jsx
@@ -1,43 +1,73 @@
 /** =========================================================================
  * FacetControllerView.jsx
  *
- * CalenderView
- * CalenderHeadView
- * CalenderTableView
- * CalenderTableHeadView
- * CalenderTableBodyView
- * NoteView
- * NoteHeadView
- * NoteListView
- * NoteListItemView
- * NoteFootView
- *
  * @author Franky Martinez <frankymartz@gmail.com>
  * @copyright FrankyMartz 2014
  * ========================================================================== */
 
-var React = require('react');
+var React = require('react/addons');
 var Calendar = require('./CalendarView.jsx');
 var Notes = require('./NoteView.jsx');
 var NoteStore = require('../stores/NoteStore.jsx');
+var createDateID = require('../utils/DateUtils.jsx').createDateID;
 
+/* Helper Functions ----------------------------------------------------------*/
+function getNoteStoreState(){
+  return {
+    allNotes: NoteStore.getAll()
+  }
+}
+
+function getNoteList(listID, notes) {
+  var list = [];
+  var note, id;
+  for (id in notes) {
+    if (notes.isOwnProperty(id)) {
+      note = notes[id];
+      if (note.listID === listID) {
+        list.push(note);
+      }
+    }
+  }
+  return list;
+}
+
+
+/* Facet App -----------------------------------------------------------------*/
 var FacetApp = React.createClass({
+
+  mixins: [React.addons.LinkedStateMixin],
 
   getInitialState: function(){
     return {
       allNotes: NoteStore.getAll(),
-      daySelected: Date.now()
+      daySelected: createDateID()
     };
   },
 
+  componentDidMount: function(){
+    NoteStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function(){
+    NoteStore.removeChangeListener(this._onChange);
+  },
+
   render: function(){
+    var noteList = getNoteList(this.state.daySelected, this.state.allNotes);
     return (
       <div className="app">
         <Calendar />
-        <Notes />
+        <Notes notes={noteList} daySelected={this.state.daySelected} />
       </div>
     );
+  },
+        //<Calendar allNotes={this.state.allNotes} daySelectedLink={this.linkState('daySelected')} />
+
+  _onChange: function(){
+    this.setState(getNoteStoreState());
   }
+
 });
 
 module.exports = FacetApp;
